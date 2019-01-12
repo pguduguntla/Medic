f3d5813278a7dacd7b78d51d3a9a3f18e626f592
praneethguduguntla <praneethg347@gmail.com>
2019-01-12 14:34:58 -0800

    added patient signup

--- app/src/main/java/com/example/praneethguduguntla/medic/MainActivity.java
+++ app/src/main/java/com/example/praneethguduguntla/medic/MainActivity.java
@@ -1,13 +1,41 @@
 package com.example.praneethguduguntla.medic;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button patient;
+    private Button doctor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        patient = (Button) findViewById(R.id.patient);
+        doctor = (Button) findViewById(R.id.doctor);
+
+
+        patient.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), PatientSignUpActivity.class);
+                startActivity(i);
+            }
+        });
+
+        doctor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), DoctorSignUpActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/praneethguduguntla/medic/PatientSignUpActivity.java
@@ -0,0 +1,74 @@
+package com.example.praneethguduguntla.medic;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class PatientSignUpActivity extends AppCompatActivity {
+
+    private Button button;
+    private String name;
+    private String email;
+    private String password;
+    private String code;
+    private FirebaseAuth mAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_sign_up);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        button = (Button) findViewById(R.id.button);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                name = ((EditText) findViewById(R.id.name)).getText().toString();
+                email = ((EditText) findViewById(R.id.email)).getText().toString();
+                password = ((EditText) findViewById(R.id.password)).getText().toString();
+                code = ((EditText) findViewById(R.id.code)).getText().toString();
+                createPatient(email, password);
+            }
+        });
+
+
+    }
+
+    public void createPatient(String email, String password){
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("hello", "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("hello", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(PatientSignUpActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+
+                        // ...
+                    }
+                });
+
+    }
+}
